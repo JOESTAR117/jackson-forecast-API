c5bc4991afdf80e5dde1d50368f571abb6d19d8b
Jackson Rios <juniorrios1231@gmail.com>
2023-01-24 22:16:34 -0300

    :heavy_plus_sign: build: setting the prettier

--- src/server.ts
+++ src/server.ts
@@ -4,25 +4,25 @@ import express from 'express';
 import { ForecastController } from './controllers/forecast';
 
 export class SetupServer extends Server {
-    constructor(private port = 3000){
-        super();
-    }
+  constructor(private port = 3000) {
+    super();
+  }
 
-    public init(): void {
-        this.setupExpress();
-        this.setupControllers();
-    }
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-    private setupExpress(): void {
-        this.app.use(express.json())
-    }
+  private setupExpress(): void {
+    this.app.use(express.json());
+  }
 
-    private setupControllers(): void{
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    }
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    public getApp(): express.Application {
-        return this.app
-    }
+  public getApp(): express.Application {
+    return this.app;
+  }
 }

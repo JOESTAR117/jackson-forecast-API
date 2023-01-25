8b9047cd44b9017325f347eb66582adc6a0a0408
Jackson Rios <juniorrios1231@gmail.com>
2023-01-25 14:25:07 -0300

    :sparkles: feat: consuming mocked and normalized api PART 2 finalized

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,18 +1,19 @@
 import { StormGlass } from '@src/clients/stormGlass';
-import axios from 'axios';
+import axios, { Axios } from 'axios';
 import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 import stormGlassWeather3hoursFixture from '@test/fixtures/stormGlass_weather_3_hours.json';
 
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('should return the normalize forecast front the StormGlass service', async () => {
     const lat = 33.792726;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3hoursFixture);
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3hoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
